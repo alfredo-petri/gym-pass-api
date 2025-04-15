e7d0c247bb17fcbf6940eea6e4b6c629d0606b70
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-15 07:17:30 -0300

    chore: run linter and fix style issues

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
 const port = env.PORT
 
 const start = async () => {
     try {
-      await app.listen({
-        host: '0.0.0.0',
-        port
-    })
-    console.log(`server is runnig on ${port} port`)  
+        await app.listen({
+            host: '0.0.0.0',
+            port,
+        })
+        console.log(`server is runnig on ${port} port`)
     } catch (err) {
-      app.log.error(err)
-      process.exit(1)
+        app.log.error(err)
+        process.exit(1)
     }
-  }
-    
-  start()
+}
+
+start()
