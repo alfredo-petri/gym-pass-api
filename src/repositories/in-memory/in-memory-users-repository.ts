b753e8369cd524097ff311cb1c7d7682fb88d7de
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-22 10:41:11 -0300

    feat(tests): create in-memory users registration for unit tests

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,26 @@
+import { UsersRepository } from '@/types/repositories-types/user-repository.js'
+import { User, Prisma } from '@prisma/client'
+
+export class InMemoryUsersRepository implements UsersRepository {
+    public users: User[] = []
+
+    async findByEmail(email: string) {
+        const user = this.users.find((user) => user.email === email)
+        if (user) return user
+        return null
+    }
+
+    async create(data: Prisma.UserCreateInput) {
+        const user = {
+            id: '1',
+            name: data.name,
+            email: data.email,
+            passwordHash: data.passwordHash,
+            createdAt: new Date(),
+        }
+
+        this.users.push(user)
+
+        return this.users[0]
+    }
+}
--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,23 +1,13 @@
 import { expect, describe, it } from 'vitest'
 import { UserRegisterUseCase } from './register.js'
 import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository.js'
 
 describe('register user use case', () => {
     it('should hash user password upon user registration', async () => {
-        const registerUseCase = new UserRegisterUseCase({
-            create: async (data) => {
-                return {
-                    id: '1',
-                    name: data.name,
-                    email: data.email,
-                    passwordHash: data.passwordHash,
-                    createdAt: new Date(),
-                }
-            },
-            findByEmail: async (_email) => {
-                return null
-            },
-        })
+        const usersRepository = new InMemoryUsersRepository()
+
+        const registerUseCase = new UserRegisterUseCase(usersRepository)
 
         const { user } = await registerUseCase.createNewUser({
             name: 'John Doe',
