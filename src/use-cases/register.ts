41b1971fc2e47bf697d6051be2f225fd20403815
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-22 10:14:18 -0300

    feat: return user on user registration

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from '@/types/repositories-types/user-repository.js'
 import { AppError } from '@/utils/AppError.js'
 import { emailInUse } from '@/utils/db-queries-errors.js'
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 
 interface IRegisterUser {
@@ -9,10 +10,18 @@ interface IRegisterUser {
     password: string
 }
 
+export interface IRegisterUserResponse {
+    user: User
+}
+
 export class UserRegisterUseCase {
     constructor(private usersRepository: UsersRepository) {}
 
-    async createNewUser({ email, name, password }: IRegisterUser) {
+    async createNewUser({
+        email,
+        name,
+        password,
+    }: IRegisterUser): Promise<IRegisterUserResponse> {
         const passwordHash = await hash(password, 6)
 
         const emailExists = await this.usersRepository.findByEmail(email)
@@ -21,6 +30,12 @@ export class UserRegisterUseCase {
             throw new AppError(emailInUse, 409)
         }
 
-        await this.usersRepository.create({ email, name, passwordHash })
+        const user = await this.usersRepository.create({
+            email,
+            name,
+            passwordHash,
+        })
+
+        return { user }
     }
 }
