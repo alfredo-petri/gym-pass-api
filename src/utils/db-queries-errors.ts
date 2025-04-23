cba1b8fbd6adae548b23c4eb7075020f734eaa26
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-23 07:32:25 -0300

    feat: improve error messages

--- src/utils/db-queries-errors.ts
+++ src/utils/db-queries-errors.ts
@@ -1,7 +1,7 @@
 export const notFound = (resource: string): string =>
     `${resource} with the provided identifier not founded`
 export const emailInUse = 'email is already in use'
-export const credentialsError = 'email or password invalid'
+export const credentialsError = 'invalid credentials'
 export const invalidToken = 'invalid token, unauthorized'
 export const requiredToken = 'token is required'
 export const unauthorized = 'unauthorized'
