2fd9a180e644cd7327d7693e1863e0d49929a0d4
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-23 08:05:25 -0300

    test: ensure user can authenticate with valid credentials

---
+++ src/use-cases/authenticate/authenticate.spec.ts
@@ -0,0 +1,27 @@
+import { expect, describe, it } from 'vitest'
+import { hash } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository.js'
+import { AuthenticateUseCase } from './authenticate.js'
+
+describe('authenticate use case', () => {
+    const password = 'Senha1234!'
+    const email = 'johndoe@email.com'
+
+    it('should be able to authenticate', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const ust = new AuthenticateUseCase(usersRepository)
+
+        await usersRepository.createNewUser({
+            name: 'John Doe',
+            email,
+            passwordHash: await hash(password, 6),
+        })
+
+        const { user } = await ust.authenticateLogin({
+            email: 'johndoe@email.com',
+            password,
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+    })
+})
