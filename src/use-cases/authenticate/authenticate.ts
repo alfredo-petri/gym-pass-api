6ba23fc4a856cfd10e32e6b03e7f29e0062c56e7
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-23 07:33:10 -0300

    feat: authenticate use case

---
+++ src/use-cases/authenticate/authenticate.ts
@@ -0,0 +1,32 @@
+import { UsersRepository } from '@/types/repositories-types/user-repository.js'
+import { AppError } from '@/utils/app-error.js'
+import { credentialsError } from '@/utils/db-queries-errors.js'
+import { User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+
+interface AuthenticateUseCaseRequest {
+    email: string
+    password: string
+}
+interface AuthenticateUseCaseResponse {
+    user: User
+}
+
+export class AuthenticateUseCase {
+    constructor(private usersRepository: UsersRepository) {}
+    async authenticate({
+        email,
+        password,
+    }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+        const user = await this.usersRepository.findUserByEmail(email)
+        if (!user) {
+            throw new AppError(credentialsError, 401)
+        }
+        const doesPasswordMatches = await compare(password, user.passwordHash)
+        if (!doesPasswordMatches) {
+            throw new AppError(credentialsError, 401)
+        }
+
+        return { user }
+    }
+}
