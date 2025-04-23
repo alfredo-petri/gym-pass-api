d7e2625708476e6a3bda2c2abbf08b4f46935cb1
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-23 08:33:16 -0300

    test: ensure authentication fails with incorrect password

--- src/use-cases/authenticate/authenticate.spec.ts
+++ src/use-cases/authenticate/authenticate.spec.ts
@@ -1,8 +1,7 @@
 import { expect, describe, it } from 'vitest'
-import { compare, hash } from 'bcryptjs'
+import { hash } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository.js'
 import { AuthenticateUseCase } from './authenticate.js'
-import { AppError } from '@/utils/app-error.js'
 
 describe('authenticate use case', () => {
     const password = 'Senha1234!'
@@ -33,6 +32,29 @@ describe('authenticate use case', () => {
             ust.authenticateLogin({
                 email,
                 password,
+            }),
+        ).rejects.toEqual(
+            expect.objectContaining({
+                message: 'invalid credentials',
+                statusCode: 401,
+            }),
+        )
+    })
+
+    it('should not be able to authenticate with wrong password', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const ust = new AuthenticateUseCase(usersRepository)
+
+        await usersRepository.createNewUser({
+            name: 'John Doe',
+            email,
+            passwordHash: await hash(password, 6),
+        })
+
+        await expect(
+            ust.authenticateLogin({
+                email: 'johndoe@email.com',
+                password: 'wrong-password',
             }),
         ).rejects.toEqual(
             expect.objectContaining({
