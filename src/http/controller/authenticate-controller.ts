1823df3c4935bf2b1f2e774cc19929f3715bb844
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-23 13:19:28 -0300

    feat: apply schemas

--- src/http/controller/authenticate-controller.ts
+++ src/http/controller/authenticate-controller.ts
@@ -1,18 +1,13 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.js'
+import { sessionSchema } from '@/schemas/session/session-schema.js'
 import { AuthenticateUseCase } from '@/use-cases/authenticate/authenticate.js'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import z from 'zod'
 
 export async function authenticateController(
     request: FastifyRequest,
     reply: FastifyReply,
 ) {
-    const authenticateBodySchema = z.object({
-        email: z.string().email(),
-        password: z.string(),
-    })
-
-    const data = authenticateBodySchema.parse(request.body)
+    const data = sessionSchema.parse(request.body)
 
     const usersRepository = new PrismaUsersRepository()
     const authenticateUseCase = new AuthenticateUseCase(usersRepository)
--- src/http/controller/register-controller.ts
+++ src/http/controller/register-controller.ts
@@ -1,20 +1,14 @@
 // import { EmailAlreadyExistsError } from '@/use-cases/errors/email-already-exists-error'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.js'
+import { registerSchema } from '@/schemas/register/register-schema.js'
 import { RegisterUseCase } from '@/use-cases/register/register.js'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import z from 'zod'
 
 export async function registerController(
     request: FastifyRequest,
     reply: FastifyReply,
 ) {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string(),
-    })
-
-    const data = registerBodySchema.parse(request.body)
+    const data = registerSchema.parse(request.body)
 
     // try {
     const usersRepository = new PrismaUsersRepository()
