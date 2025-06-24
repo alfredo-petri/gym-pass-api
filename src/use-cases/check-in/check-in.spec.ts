7edb7250937f27d41b14d5c5831597664ba606ea
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-24 16:36:51 -0300

    feat: use vi fake timers to mock date

--- src/use-cases/check-in/check-in.spec.ts
+++ src/use-cases/check-in/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from './check-in.js'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository.js'
 
@@ -9,13 +9,22 @@ describe('checkin use case', () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository()
         sut = new CheckInUseCase(checkInsRepository)
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        vi.useRealTimers()
     })
 
     it('should be able to check in', async () => {
+        vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
         const { checkIn } = await sut.createNewCheckIn({
             gymId: 'gym-01',
             userId: 'user-01',
         })
+
+        console.log(checkIn.createdAt)
 
         expect(checkIn.id).toEqual(expect.any(String))
     })
